bce045bf07db642418849a1bc52cb83b22676e02
web-Chen <34672368+coltsfoots@users.noreply.github.com>
2019-02-16 01:26:12 +0800

    Update move.js
    整理下格式

--- move.js
+++ move.js
@@ -1,40 +1,40 @@
 function startMove(obj, json, fn) {
-	if( typeof json != Object){
-		throw new Error(json+' is not a Object')
-		return
-	}
-	clearInterval(obj.timer)
-	var iCur = 0
-	var iSpeed = 0;
-	obj.timer = setInterval(() => {
-		var flag = true
-		for( var attr in json){
-			var iTarget = json[attr]
-			if( attr == 'opacity'){
-				iCur = Math.round(css(obj,'opacity')*100)
-			}else{
-				iCur = parseInt(css(obj, attr))
-			}
+  if( typeof json != Object){
+    throw new Error(json+' is not a Object')
+    return
+  }
+  clearInterval(obj.timer)
+  var iCur = 0
+  var iSpeed = 0;
+  obj.timer = setInterval(() => {
+    var flag = true
+    for( var attr in json){
+      var iTarget = json[attr]
+      if( attr == 'opacity'){
+        iCur = Math.round(css(obj,'opacity')*100)
+      }else{
+        iCur = parseInt(css(obj, attr))
+      }
 
-			iSpeed = (iTarget - iCur)/8
+      iSpeed = (iTarget - iCur)/8
 
-			iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed)
+      iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed)
 
-			if (iCur != iTarget) {
-				flag = false
-				if( attr == 'opacity'){
-					obj.style.opacity = (iCur + iSpeed)/100
-					obj.style.filter = 'alpha(opacity='+ (iCur + iSpeed) +')'
-				}else{
-					obj.style[attr] = iCur + iSpeed + 'px'
-				}
-			}
-		}
-		if( flag ){
-			clearInterval(obj.timer)
-			fn && fn.call(obj)
-		}
-	}, 30)
+      if (iCur != iTarget) {
+        flag = false
+        if( attr == 'opacity'){
+          obj.style.opacity = (iCur + iSpeed)/100
+          obj.style.filter = 'alpha(opacity='+ (iCur + iSpeed) +')'
+        }else{
+          obj.style[attr] = iCur + iSpeed + 'px'
+        }
+      }
+    }
+    if( flag ){
+      clearInterval(obj.timer)
+      fn && fn.call(obj)
+    }
+  }, 30)
 }
 function css(obj,attr){
 	if(obj.currentStyle){
